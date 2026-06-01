a7bc6d977cfea45eef871938e6f4c20f3d3dfa28
pactify-app-india <pactify.in@gmail.com>
2026-06-01 13:46:50 +0530

    feat: wire Auth0 and AppAuth providers into main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import Auth0ProviderWithNavigate from './auth/Auth0ProviderWithNavigate';
+import { AppAuthProvider } from './auth/AppAuthProvider';
+import AppRouter from './routing/AppRouter';
+import './index.css';
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+  <React.StrictMode>
+    <BrowserRouter>
+      <Auth0ProviderWithNavigate>
+        <AppAuthProvider>
+          <AppRouter />
+        </AppAuthProvider>
+      </Auth0ProviderWithNavigate>
+    </BrowserRouter>
+  </React.StrictMode>
+);
